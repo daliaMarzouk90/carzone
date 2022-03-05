7b69696f3e74ca32a6f8c60f12a8d63f7d30ff9d
dalia <dalia.m.refaie@gmail.com>
2022-03-05 19:17:11 +0200

    cars model

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import car
+from django.utils.html import format_html
+
+class ModelCar(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<image src="{}" width="40" style="border-radius: 50px;"/>'.format(object.car_photo.url))
+    
+    thumbnail.short_description = 'photo'
+
+    list_display = ("id", "car_title", "thumbnail", "price", "created_date")
+    list_display_links = ("id", "car_title")
+    list_filter = ("car_title","price", "created_date")
+    search_fields = ("model",)
 
 # Register your models here.
+admin.site.register(car, ModelCar)
